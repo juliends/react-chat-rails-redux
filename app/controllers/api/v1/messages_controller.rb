6bb795d23402aeb711f151e2b0f0e267ab9ae00b
Julien Da Silva <julien.da.silva@hotmail.com>
2017-09-29 11:50:13 +0200

    api index

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,7 +1,18 @@
 class Api::V1::MessagesController < ApplicationController
+  
+  before_action :set_channel
+  
   def index
+    @messages = Message.where(channel: @channel)
+    render json: @messages
   end
 
   def create
+  end
+
+  private
+
+  def set_channel
+    @channel = Channel.first
   end
 end
