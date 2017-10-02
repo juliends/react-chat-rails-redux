e5d11cd73ea9e803e182d526eb85d194392b7460
Julien Da Silva <julien.da.silva@hotmail.com>
2017-10-02 11:45:37 +0200

    trying to rebuild yarn

--- app/javascript/chat/components/app.jsx
+++ app/javascript/chat/components/app.jsx
@@ -6,7 +6,7 @@ const App = (props) => {
   return (
     <div className="messaging-wrapper">
       <div className="logo-container">
-        <img className="messaging-logo" src="assets/images/logo.svg" />
+        <img className="messaging-logo fa fa-star" src="" />
       </div>
       <ChannelList selectedChannel={props.match.params.channel} />
       <MessageList selectedChannel={props.match.params.channel} />
